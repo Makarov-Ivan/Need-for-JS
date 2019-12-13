c9f68b1c5d53c003607a2e794fbe428eaf06951f
Makarov-Ivan <makivanik@gmail.com>
2019-12-13 19:11:03 +0300

    end of 1st lesson

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
-const score = document.querySelector('.score'),
-    start = document.querySelector('.start'),
-    gameArea = document.querySelector('.gameArea'),
-    car = document.createElement('div');
+const score = document.querySelector(".score"),
+    start = document.querySelector(".start"),
+    gameArea = document.querySelector(".gameArea"),
+    car = document.createElement("div");
 const keys = {
     ArrowUp: false,
     ArrowDown: false,
     ArrowRight: false,
     ArrowLeft: false,
-}
+};
 const setting = {
     start: false,
     score: 0,
     speed: 3,
-}
-car.classList.add('car')
-start.addEventListener('click', startGame);
-document.addEventListener('keydown', startRun);
-document.addEventListener('keyup', stopRun);
+};
+car.classList.add("car");
+start.addEventListener("click", startGame);
+document.addEventListener("keydown", startRun);
+document.addEventListener("keyup", stopRun);
 
 function startGame() {
-    start.classList.add('hide');
+    start.classList.add("hide");
     setting.start = true;
-    gameArea.appendChild(car)
+    gameArea.appendChild(car);
     requestAnimationFrame(playGame);
 }
 
 function playGame() {
-    console.log('playGame!');
+    console.log("playGame!");
     if (setting.start) {
         requestAnimationFrame(playGame);
     }
